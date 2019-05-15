239b20838c3ccd479339810432373c1c179ada2c
John Ryan <john.ryan.a.angeles@gmail.com>
2019-05-15 14:42:42 +0800

    Test

---
+++ Demo/src/demo/pack/Demo.java
@@ -0,0 +1,54 @@
+package demo.pack;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+public class Demo {
+	
+	static WebDriver driver;
+	private static String baseUrl;
+	
+	public static void setup() throws Exception{
+		
+		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_DRIVER);
+		
+		File pathToFirefoxBinary = new File(Util.FIREFOX_PATH);
+		FirefoxBinary firefoxBinary = new FirefoxBinary(pathToFirefoxBinary);
+		
+		FirefoxProfile fireFoxProfile = new FirefoxProfile();
+		FirefoxOptions fireFoxOptions = new FirefoxOptions();
+		
+		fireFoxOptions.setBinary(firefoxBinary);
+		fireFoxOptions.setProfile(fireFoxProfile);
+		
+		driver = new FirefoxDriver(fireFoxOptions);
+		
+		baseUrl = Util.BASE_URL;
+		
+		driver.manage().timeouts().implicitlyWait(Util.WAIT_TIME, TimeUnit.SECONDS);
+		
+		driver.get(baseUrl + "/V4/");
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		setup();
+		
+		driver.findElement(By.name("uid")).clear();
+		driver.findElement(By.name("uid")).sendKeys(Util.USER_NAME);
+		
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys(Util.PASSWD);
+		
+		driver.findElement(By.name("btnLogin")).click();
+		
+	}
+}
---
+++ Demo/src/demo/pack/Util.java
@@ -0,0 +1,23 @@
+package demo.pack;
+
+public class Util {
+	
+	/* You can change the Path of FireFox based on your environment here */
+	public static final String FIREFOX_PATH = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+	
+	public static final String FIREFOX_DRIVER = "C:\\Users\\Goo\\Desktop\\Selenium\\geckodriver.exe";
+	
+	// Setting Base URL
+	public static final String BASE_URL = "http://www.demo.guru99.com/";
+    
+	// Time to wait when searching for a GUI element 
+	public static final int WAIT_TIME = 30; 
+											
+	// Valid account for login
+	public static final String USER_NAME = "mngr193949";
+	public static final String PASSWD = "ytEhAgu";
+
+	// Expected output
+	public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
+	
+}
