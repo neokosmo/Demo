b41d3bc1a05b8fdde069a080c6a41d6701908582
John Ryan <jrangeles.ph1@gmail.com>
2019-05-15 15:29:51 +0800

    Maven Dependencies

--- Demo/src/demo/pack/Demo.java
+++ Demo/src/demo/pack/Demo.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -17,8 +19,13 @@ public class Demo {
 	static WebDriver driver;
 	private static String baseUrl;
 	
+	@AfterTest
+	public void termianteBrowser() {
+		driver.close();
+	}
+	
 	@BeforeTest
-	public static void setup() throws Exception{
+	public void launchBrowser() throws Exception{
 		
 		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_DRIVER);
 		
@@ -42,7 +49,7 @@ public class Demo {
 	}
 	
 	@Test
-	public static void userLogIn() throws Exception {
+	public void userLogIn() throws Exception {
 		
 		driver.findElement(By.name("uid")).clear();
 		driver.findElement(By.name("uid")).sendKeys(Util.USER_NAME);
@@ -51,6 +58,8 @@ public class Demo {
 		driver.findElement(By.name("password")).sendKeys(Util.PASSWD);
 		
 		driver.findElement(By.name("btnLogin")).click();
+		String actualTitle = driver.getTitle();
 		
+		Assert.assertEquals(actualTitle, Util.EXPECT_TITLE);
 	}
 }
