45e240691883581cf3c1e1a1798ca5dcdc9e15a4
John Ryan <jrangeles.ph1@gmail.com>
2019-05-15 15:03:26 +0800

    With Test NG

--- Demo/src/demo/pack/Demo.java
+++ Demo/src/demo/pack/Demo.java
@@ -9,12 +9,15 @@ import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Demo {
 	
 	static WebDriver driver;
 	private static String baseUrl;
 	
+	@BeforeTest
 	public static void setup() throws Exception{
 		
 		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_DRIVER);
@@ -38,9 +41,8 @@ public class Demo {
 		
 	}
 	
-	public static void main(String[] args) throws Exception {
-		
-		setup();
+	@Test
+	public static void userLogIn() throws Exception {
 		
 		driver.findElement(By.name("uid")).clear();
 		driver.findElement(By.name("uid")).sendKeys(Util.USER_NAME);
