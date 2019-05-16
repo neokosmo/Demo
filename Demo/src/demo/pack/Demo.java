0f4c5c8b3e3bfa08e4a5de789429b303d8eb20a7
John Ryan <jrangeles.ph1@gmail.com>
2019-05-16 12:10:18 +0800

    Data Provider

--- Demo/src/demo/pack/Demo.java
+++ Demo/src/demo/pack/Demo.java
@@ -1,65 +1,84 @@
 package demo.pack;
 
+import static org.testng.Assert.assertEquals;
+
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Demo {
-	
+
 	static WebDriver driver;
-	private static String baseUrl;
+	private String baseUrl;
 	
 	@AfterTest
-	public void termianteBrowser() {
+	public void closeBrowser() {
 		driver.close();
 	}
 	
-	@BeforeTest
-	public void launchBrowser() throws Exception{
-		
-		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_DRIVER);
-		
-		File pathToFirefoxBinary = new File(Util.FIREFOX_PATH);
-		FirefoxBinary firefoxBinary = new FirefoxBinary(pathToFirefoxBinary);
-		
-		FirefoxProfile fireFoxProfile = new FirefoxProfile();
-		FirefoxOptions fireFoxOptions = new FirefoxOptions();
-		
-		fireFoxOptions.setBinary(firefoxBinary);
-		fireFoxOptions.setProfile(fireFoxProfile);
-		
-		driver = new FirefoxDriver(fireFoxOptions);
-		
-		baseUrl = Util.BASE_URL;
-		
-		driver.manage().timeouts().implicitlyWait(Util.WAIT_TIME, TimeUnit.SECONDS);
-		
-		driver.get(baseUrl + "/V4/");
-		
+	@DataProvider(name="testData")
+	public Object[][] testData() throws Exception {
+		return Util.getDataFromExcel(Util.XLSPATH, Util.SHEET_NAME, Util.TABLE_NAME);
 	}
 	
-	@Test
-	public void userLogIn() throws Exception {
-		
-		driver.findElement(By.name("uid")).clear();
-		driver.findElement(By.name("uid")).sendKeys(Util.USER_NAME);
-		
-		driver.findElement(By.name("password")).clear();
-		driver.findElement(By.name("password")).sendKeys(Util.PASSWD);
-		
-		driver.findElement(By.name("btnLogin")).click();
-		String actualTitle = driver.getTitle();
-		
-		Assert.assertEquals(actualTitle, Util.EXPECT_TITLE);
+	@BeforeTest
+	public void launchBrowser() throws Exception {
+
+		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_DRIVER);
+
+		File pathToFirefoxBinary = new File(Util.FIREFOX_PATH);
+		FirefoxBinary firefoxBinary = new FirefoxBinary(pathToFirefoxBinary);
+
+		FirefoxProfile fireFoxProfile = new FirefoxProfile();
+		FirefoxOptions fireFoxOptions = new FirefoxOptions();
+
+		fireFoxOptions.setBinary(firefoxBinary);
+		fireFoxOptions.setProfile(fireFoxProfile);
+
+		driver = new FirefoxDriver(fireFoxOptions);
+
+		baseUrl = Util.BASE_URL;
+
+		driver.manage().timeouts().implicitlyWait(Util.WAIT_TIME, TimeUnit.SECONDS);
+
+		driver.get(baseUrl + "/V4/");
+
+	}
+
+	@Test(dataProvider="testData")
+	public void userLogIn(String username,String password) throws Exception {
+		String actualTitle;
+		String actualBoxtitle;
+
+			driver.findElement(By.name("uid")).clear();
+			driver.findElement(By.name("uid")).sendKeys(username);
+
+			driver.findElement(By.name("password")).clear();
+			driver.findElement(By.name("password")).sendKeys(password);
+
+			driver.findElement(By.name("btnLogin")).click();
+			
+			try {
+				Alert alt = driver.switchTo().alert();
+				actualBoxtitle = alt.getText(); // get content of the Alter Message
+				alt.accept();
+				assertEquals(actualBoxtitle,Util.EXPECT_ERROR);
+			} catch (NoAlertPresentException Ex) {
+				actualTitle = driver.getTitle();
+				// On Successful login compare Actual Page Title with Expected Title
+				assertEquals(actualTitle,Util.EXPECT_TITLE);
+		}
 	}
 }
--- Demo/src/demo/pack/Util.java
+++ Demo/src/demo/pack/Util.java
@@ -1,7 +1,16 @@
 package demo.pack;
 
+import java.io.File;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
 public class Util {
 	
+	public static final String XLSPATH = "testData.xls";
+	public static final String SHEET_NAME = "Data"; // Sheet name
+	public static final String TABLE_NAME = "testData"; // Name of data table
 	/* You can change the Path of FireFox based on your environment here */
 	public static final String FIREFOX_PATH = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
 	
@@ -13,11 +22,50 @@ public class Util {
 	// Time to wait when searching for a GUI element 
 	public static final int WAIT_TIME = 30; 
 											
-	// Valid account for login
-	public static final String USER_NAME = "mngr193949";
-	public static final String PASSWD = "ytEhAgu";
-
 	// Expected output
 	public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
+    public static final String EXPECT_ERROR = "User or Password is not valid";
+	
+	public static String[][] getDataFromExcel(String xlFilePath,
+			String sheetName, String tableName) throws Exception {
+		// Declare a 2 dimensions array to store all the test data read from
+		// excel
+		String[][] tabArray = null;
+
+		// get the workbook file. Provide the path of excel file
+		Workbook workbook = Workbook.getWorkbook(new File(xlFilePath));
+		// get the sheet name
+		Sheet sheet = workbook.getSheet(sheetName);
+
+		int startRow, startCol, endRow, endCol, ci, cj;
+
+		// find cell position which contain first appear table name
+		Cell tableStart = sheet.findCell(tableName);
+		// Row position of FIRST appear table name
+		startRow = tableStart.getRow();
+		// Col position of FIRST appear table name
+		startCol = tableStart.getColumn();
+
+		// find cell position which contain last appear table name
+		Cell tableEnd = sheet.findCell(tableName + "End");
+		// Row position of LAST appear table name
+		endRow = tableEnd.getRow();
+		// Col position of LAST appear table name
+		endCol = tableEnd.getColumn();
+
+		tabArray = new String[endRow - startRow - 1][endCol - startCol - 1];
+		ci = 0;
+
+		// Store all data in an array
+		for (int i = startRow + 1; i < endRow; i++, ci++) {
+			cj = 0;
+			for (int j = startCol + 1; j < endCol; j++, cj++) {
+				//Get content of each cell and store to each array element.
+				tabArray[ci][cj] = sheet.getCell(j, i).getContents();
+			}
+		}
+
+		return (tabArray);
+	}
 	
 }
